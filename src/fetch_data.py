4346a1c6599092aa97c37994b68f7592cdcc6b34
Kwokym111 <kwok.yen.ming06@gmail.com>
2026-06-30 00:09:07 +0800

    added extra comments in fetch_data.py

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -11,11 +11,12 @@ def generate_data(n_records=810_909, seed=42):
     block_ids = np.arange(n_records)
     # number of transactions in a block
     n_transactions = rng.integers(1, 6_936, size=n_records)
-    # size of a block (max of 2MB)
+    # size of a block in bytes (max of 2MB)
     size = rng.integers(215, 2_000_001, size=n_records)
     # difficulty of the hash required for PoW (exponentially increasing)
     difficulty = np.linspace(1, 1e13, n_records)
     # amount of BTC moved in a block (eg: 50 transactions of 2BTC = 100 transaction_volume)
+    # 0 BTC to 21 million BTC (MAX amount to exist)
     transaction_volume = rng.uniform(0, 2_100_000, size=n_records)
     
     df = pd.DataFrame({
